5fad0db55a5658397facefa945c513318b34b8d2
Elena Rubilova <yelena.rubilova@gmail.com>
2024-10-15 11:34:24 -0400

    Guarantee we do not deliver a result (invoke the completion closure) after the RemoteFeedLoader instance has been deallocated

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -27,7 +27,10 @@ public class RemoteFeedLoader {
     }
     
     public func load(completion: @escaping (Result) -> Void ) {
-        client.get(from: url, completion: { result in
+        client.get(from: url, completion: { [weak self] result in
+            guard self != nil else {
+                return
+            }
             switch result {
                 case let .success(data, response):
                 completion(FeedItemsMapper.map(data, from: response))
